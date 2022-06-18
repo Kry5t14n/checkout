f50e0286ed0c3f6b8fdae2cb35c36590323bf706
Kry5t14n <savcio9182@gmail.com>
2022-06-18 13:46:04 +0200

    update

---
+++ public/js/index.js
@@ -0,0 +1,6 @@
+const names = document.getElementsByClassName('item-name')
+const prices = document.getElementsByClassName('item-price')
+const counters = document.getElementsByClassName('item-counter')
+const btns_less = document.getElementsByClassName('item-btn-less')
+const btns_more = document.getElementsByClassName('item-btn-more')
+
